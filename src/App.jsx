409f75d6957aff94dd839bba0eff48ba52376a8d
Shubham Kumar <shubhams9771@example.com>
2026-08-16 20:00:18 +0530

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       element: <Navbar />,
       children: [
         {
-          index: trues,
+          index: true,
           element: <Createbooks />,
         },
         {
